0d1c036febf1625389b06f6445a252fdf8183cc6
mo bhallil <is.bhallil@gmail.com>
2019-10-13 14:40:15 -0400

    generate employee that belongs_to :user

---
+++ db/migrate/20191013183441_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+      t.string :email
+      t.string :encrypted_password
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_183209) do
+ActiveRecord::Schema.define(version: 2019_10_13_183441) do
 
   create_table "awards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "building_type"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 2019_10_13_183209) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "title"
+    t.string "email"
+    t.string "encrypted_password"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_employees_on_user_id"
   end
 
   create_table "navs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -55,4 +67,5 @@ ActiveRecord::Schema.define(version: 2019_10_13_183209) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "employees", "users"
 end
