785e9d58c50ba9084eb7c4562b5eb5eed577a1ea
EtienneBosse <54867761+EtienneBosse@users.noreply.github.com>
2019-10-29 11:10:18 -0400

    first_email_received

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 #SendGrid
 ActionMailer::Base.smtp_settings = {
-    :user_name => '	apikey',
+    :user_name => 'apikey',
     :password => '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',
     :domain => 'rocketgb.best',
     :address => 'smtp.sendgrid.net',
