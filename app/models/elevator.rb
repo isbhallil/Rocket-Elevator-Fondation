09a1d8b33e9020ac15e44f0a5ca24e494d692af0
sarahbech <sarah_bechik@hotmail.com>
2019-10-28 20:47:40 -0400

    smsSending

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,27 +1,29 @@
-require 'twilio-ruby'
 
 class Elevator < ApplicationRecord
     include RailsAdminCharts
     belongs_to :column
 
-    
-    after_update :send_msg
-
-    account_sid = 'ACa53e6d98eb4354b13829d86c40f07c11'
-    auth_token = 'yd1d7f170d8adb95047075dd55dc3f30e'
+    require 'twilio-ruby'
 
     
+    after_update do |elevator|
+
+
     paramTwilio = {
-        from: '+15017122661',
+        from: '+15712976606',
         body: 'An elevator has changed status',
         to: '+33766846471'
-      }
+    }
     
-      @client = Twilio::REST::Client.new(account_sid, auth_token)
-      message = @client.messages.create(paramTwilio)                           
+    account_sid = 'ACa53e6d98eb4354b13829d86c40f07c11'
+    auth_token = 'd1d7f170d8adb95047075dd55dc3f30e'
+
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    message = @client.messages.create(paramTwilio)                         
     
       puts message.sid
 
+    end
 
     puts "Elevator status changed"
     
