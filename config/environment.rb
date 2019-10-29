d4c8ae67e6eecd72aa0e2d9d42ad9fc3efcaa26d
EtienneBosse <54867761+EtienneBosse@users.noreply.github.com>
2019-10-29 12:54:43 -0400

    new_account_new_key

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 #SendGrid
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
-    :password => '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',
+    :password => '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',
     :domain => 'rocketgb.best',
     :address => 'smtp.sendgrid.net',
     :port => 587, #was 587 initially
