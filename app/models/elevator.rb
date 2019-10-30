68fa86c2f9d55dd63e556c23e9a387a0b3c7ebcb
EtienneBosse <54867761+EtienneBosse@users.noreply.github.com>
2019-10-30 14:43:51 -0400

    slack_working_with_hidden_key

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -4,7 +4,8 @@ class Elevator < ApplicationRecord
     after_update :status_changed
     
     def status_changed
-        SlackNotifier::CLIENT.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{previous_changes[:status][0]} to #{self.status}"
+        notifier = Slack::Notifier.new(ENV['Slack_Webhook'], channel: ENV['Slack_Channel'], username: ENV['Slack_Username'])
+        notifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{previous_changes[:status][0]} to #{self.status}"
     end
 
 end
--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,3 +1,2 @@
 module SlackNotifier
-    CLIENT = Slack::Notifier.new"https://hooks.slack.com/services/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"
 end
