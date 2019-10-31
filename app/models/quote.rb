35891bdc177f721dff471748f05c0f547112ce25
sarahbech <sarah_bechik@hotmail.com>
2019-10-30 23:58:14 -0400

    quotemodelupdate

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,11 +3,13 @@ class Quote < ApplicationRecord
     include RailsAdminCharts
     #include ActiveModel::Dirty
 
-    belongs_to :lead
+        # belongs_to :lead but leads need to migrate first
 
-    after_create do |q|
-            
-            Zendesk.new.quote_ticket(self.full_name, self.business_name, self.email, self.phone_number, self.building_project_name, self.project_description, self.message, self.departement_in_charge_of_elevators)
+    after_create :quote_ticket
 
+    def quote_ticket
+        
+        Zendesk.new.quote_ticket(self.full_name, self.business_name, self.email, self.phone_number, self.building_project_name, self.project_description, self.message, self.departement_in_charge_of_elevators)
+    
     end
 end
