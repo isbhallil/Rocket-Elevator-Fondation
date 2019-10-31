455682849b2e87c7175e7344d7f0428315bc9659
mo bhallil <is.bhallil@gmail.com>
2019-10-31 11:59:19 -0400

    exemple of usecase for the apis

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,16 @@ class Customer < ApplicationRecord
     belongs_to :address, optional: true # removed dependent destroy because a user still can use it
 
     include RailsAdminCharts
-    
+
     belongs_to :user, optional: true
     has_many :buildings
-    
+    after_create :send_ticket
+
+
+    private
+    # exemple of usecase
+    def send_ticket
+        Notifier.send_ticket
+    end
+
 end
---
+++ lib/modules/notifier.rb
@@ -0,0 +1,7 @@
+module Notifier
+    # @client =  definer here your client connection with zendesk api
+
+    def self.send_ticket
+        ap "blablabla ================================"
+    end
+end
