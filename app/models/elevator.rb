da115e376dff97831d55eaa89a5b326d12421366
sarahbech <sarah_bechik@hotmail.com>
2019-11-01 13:09:53 -0400

    twilio-module-good-updated

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -10,7 +10,7 @@ class Elevator < ApplicationRecord
     building = self.column.battery.building
     updated_at = self.updated_at
     serial_number = self.serial_number
-    body = ("Hello #{building.full_name_tech_person}, elevator #{serial_number} has changed status at #{updated_at}")
+    body = ("Hello #{building.full_name_tech_person}, elevator #{serial_number} has changed status on #{updated_at}")
     to = '+33766846471'
     from = '+15712976606'
 
