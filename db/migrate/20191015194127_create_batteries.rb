c262408b3a09c49e5c2495d838c885949a00985a
mo bhallil <is.bhallil@gmail.com>
2019-11-04 11:31:41 -0500

    change migration for Batteries

--- db/migrate/20191015194127_create_batteries.rb
+++ db/migrate/20191015194127_create_batteries.rb
@@ -3,14 +3,13 @@ class CreateBatteries < ActiveRecord::Migration[5.2]
     create_table :batteries do |t|
       t.references :building, foreign_key: {on_delete: :cascade, on_update: :cascade}, null: false
       t.references :employee, foreign_key: {on_delete: :cascade, on_update: :cascade}
-      t.string :building_type, null: false
       t.string :status
       t.date :date_of_installation
       t.date :date_of_inspection
       t.string :inspection_certificate
       t.text :information
       t.text :notes
-      
+
       t.timestamps
     end
   end
