63abab2e768495f8c28c4bea25059e5b5f1c694f
mo bhallil <is.bhallil@gmail.com>
2019-11-13 14:10:47 -0500

    Commit général mercredi 13h30

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,17 +1,16 @@
 class InterventionsController < ApplicationController
 
   def new
-    @intervention = Intervention.new
-    @intervention.author = params["author_id"]
-    @intervention.customer = params["customer_id"]
-    @intervention.building = params["building_id"]
-    @intervention.battery = params["battery_id"]
-    @intervention.column = params["column_id"]
-    @intervention.elevator = params["elevator_id"]
-    @intervention.employee = params["employee_id"]
-    @intervention.report = params["repor_id"]
-
-    @intervention.try(:save!)
+    @intervention = Intervention.create({
+      author_id = params["author_id"]
+      customer_id = params["customer_id"]
+      building_id = params["building_id"]
+      battery_id = params["battery_id"]
+      column_id = params["column_id"]
+      elevator_id = params["elevator_id"]
+      employee_id = params["employee_id"]
+      report = params["report"]
+    })
   end
 
 end
